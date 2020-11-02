76f56001f19a9de25eb3dec6f28e573cf99fee6a
Andreas Neukoetter <andreas@omni-mad.com>
2020-11-02 20:19:25 +0100

    : Clean up example script.

--- ex_01.sh
+++ ex_01.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-lowtexpal -f example_01.png add-color --color "#000000"
-lowtexpal -f example_01.png add-color --color "#ffffff"
-lowtexpal -f example_01.png add-color --color "#ff0000"
-lowtexpal -f example_01.png add-color --color "#00ff00"
-lowtexpal -f example_01.png add-color --color "#0000ff"
-lowtexpal -f example_01.png add-color --color "#ffff00"
-lowtexpal -f example_01.png add-color --color "#ff00ff"
-lowtexpal -f example_01.png add-color --color "#00ffff"
+png="example_01.png"
+rm ${png}
 
+lowtexpal -f ${png} add-color --color "#000000"
+lowtexpal -f ${png} add-color --color "#ffffff"
+lowtexpal -f ${png} add-color --color "#ff0000"
+lowtexpal -f ${png} add-color --color "#00ff00"
+lowtexpal -f ${png} add-color --color "#0000ff"
+lowtexpal -f ${png} add-color --color "#ffff00"
+lowtexpal -f ${png} add-color --color "#ff00ff"
+lowtexpal -f ${png} add-color --color "#00ffff"
+
+gm convert ${png} -filter point -resize 128x128 big_${png}
